c4d02165bc11221c1d7de6b4e945f8c0fa0200f6
shivanandvp <shivanandvp@rebornos.org>
2022-01-12 08:33:06 -0600

    feat: script rewrite and reorganization, polkit policy, robust exit status return, and service improvement

--- build.sh
+++
@@ -1,5 +0,0 @@
-sudo pacman -R refresh-mirrors
-sudo rm -rf refresh-mirrors-rebornos*.pkg.*
-makepkg -f --noextract \
-&& sudo pacman -U --noconfirm *.pkg.tar.zst \
-&& gpg --detach-sign --use-agent *.pkg.tar.zst
--- refresh-mirrors-rebornos.sh
+++ refresh-mirrors-rebornos.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env sh
 
+wait_and_exit() {
+    WAIT_TIME="$1"
+    EXIT_CODE="$2"    
+
+    read -n 1 -s -r -t "$WAIT_TIME" -p "Press any key to exit..."
+    exit "$EXIT_CODE"
+}
+
+exit_if_needed() {
+    EXIT_CODE="$1"
+
+    if [ "$EXIT_CODE" -ne 0 ]; then
+        wait_and_exit 20 "$EXIT_CODE"
+    fi
+}
+
 echo "Ranking RebornOS Mirrors..."
 echo ""
 pkexec /usr/bin/rate-mirrors --concurrency=16 --per-mirror-timeout=3000 --allow-root --save=/etc/pacman.d/reborn-mirrorlist rebornos
+exit_if_needed "$?"
 echo ""
 echo ""
 
 echo "Ranking ArchLinux Mirrors..."
 echo ""
 pkexec /usr/bin/rate-mirrors --protocol=https --allow-root --save=/etc/pacman.d/mirrorlist arch
+exit_if_needed "$?"
 echo ""
 echo ""
 
-read -n 1 -s -r -t 10 -p "Press any key to exit..."; exit 0
+wait_and_exit 10
