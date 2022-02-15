cf732b786c472a1f1b879f92d50dab53ede639ca
shivanandvp <shivanandvp@rebornos.org>
2022-02-15 00:46:00 -0600

    feat: Smarter script
    Does not replace the mirrorlist files if either rate-mirrors fails to run, or if the minimum number of mirrors are not found. If the minimum number of mirrors are not found, the script retries with a longer per mirror timeout. If that too fails, nothing is overwritten. Otherwise the mirrorlists are updated.

--- refresh-mirrors.sh
+++ refresh-mirrors.sh
@@ -12,9 +12,20 @@ wait_and_exit() {
 # Refresh RebornOS Mirrors
 # ========================
 
+rank_rebornos_mirrors() {
+    TEMP_FILE="$1"
+    TIMEOUT="$2"
+
+    /usr/bin/rate-mirrors --concurrency=16 --per-mirror-timeout="$TIMEOUT" --allow-root --save="$TEMP_FILE" rebornos
+    return "$?"
+}
+
 TEMP_DIR="/tmp/pacman.d"
 DESTINATION_DIR="/etc/pacman.d"
 MIRRORLIST_FILENAME="reborn-mirrorlist"
+PER_MIRROR_TIMEOUT=3000
+FALLBACK_PER_MIRROR_TIMEOUT=10000
+MIN_MIRRORS=5
 
 TEMP_FILE="$TEMP_DIR/$MIRRORLIST_FILENAME"
 MIRRORLIST_FILE="$DESTINATION_DIR/$MIRRORLIST_FILENAME"
@@ -28,13 +39,34 @@ echo ""
 
 echo "Ranking RebornOS Mirrors..."
 echo ""
-/usr/bin/rate-mirrors --concurrency=16 --per-mirror-timeout=3000 --allow-root --save="$TEMP_FILE" rebornos
+rank_rebornos_mirrors "$TEMP_FILE" "$PER_MIRROR_TIMEOUT"
 REBORN_MIRROR_REFRESH_FAILED="$?"
+MIRROR_COUNT=$(grep -oe '^Server\s*=\s*http' "$TEMP_FILE" | wc -l)
 echo ""
 echo ""
 
+if [ "$MIRROR_COUNT" -lt "$MIN_MIRRORS" ]; then  
+    echo "Only $MIRROR_COUNT mirrors found..."
+    echo "Retrying with a longer timeout duration. This will be much slower. Please be patient..."
+    echo ""
+    rm -f "$TEMP_FILE"
+    rank_rebornos_mirrors "$TEMP_FILE" "$FALLBACK_PER_MIRROR_TIMEOUT"
+    REBORN_MIRROR_REFRESH_FAILED="$?"
+    MIRROR_COUNT=$(grep -oe '^Server\s*=\s*http' "$TEMP_FILE" | wc -l)
+    echo ""
+    echo ""
+fi
+
 REBORN_MIRROR_REFRESH_LOG_FILE="$TEMP_FILE"
-if [ "$REBORN_MIRROR_REFRESH_FAILED" -eq 0 ]; then
+if [ "$REBORN_MIRROR_REFRESH_FAILED" -ne 0 ]; then
+    echo ""  
+    echo "ERROR: rate-mirrors exited with the error code $REBORN_MIRROR_REFRESH_FAILED..."
+    echo ""  
+elif [ "$MIRROR_COUNT" -lt "$MIN_MIRRORS" ]; then
+    REBORN_MIRROR_REFRESH_FAILED=-1
+    echo "ERROR: Only $MIRROR_COUNT mirrors found even with a longer timeout duration..."
+    echo "" 
+else    
     echo ""  
     set -o xtrace      
     pkexec cp -f "$TEMP_FILE" "$MIRRORLIST_FILE"
@@ -46,9 +78,20 @@ fi
 # Refresh Arch Linux Mirrors
 # ==========================
 
+rank_arch_mirrors() {
+    TEMP_FILE="$1"
+    TIMEOUT="$2"
+
+    /usr/bin/rate-mirrors --protocol=https --per-mirror-timeout="$TIMEOUT" --allow-root --save="$TEMP_FILE" arch
+    return "$?"
+}
+
 TEMP_DIR="/tmp/pacman.d"
 DESTINATION_DIR="/etc/pacman.d"
 MIRRORLIST_FILENAME="mirrorlist"
+PER_MIRROR_TIMEOUT=15
+FALLBACK_PER_MIRROR_TIMEOUT=5000
+MIN_MIRRORS=5
 
 TEMP_FILE="$TEMP_DIR/$MIRRORLIST_FILENAME"
 MIRRORLIST_FILE="$DESTINATION_DIR/$MIRRORLIST_FILENAME"
@@ -62,13 +105,34 @@ echo ""
 
 echo "Ranking Arch Linux Mirrors..."
 echo ""
-/usr/bin/rate-mirrors --protocol=https --allow-root --save="$TEMP_FILE" arch
+rank_arch_mirrors "$TEMP_FILE" "$PER_MIRROR_TIMEOUT"
 ARCH_MIRROR_REFRESH_FAILED="$?"
+MIRROR_COUNT=$(grep -oe '^Server\s*=\s*http' "$TEMP_FILE" | wc -l)
 echo ""
 echo ""
 
+if [ "$MIRROR_COUNT" -lt "$MIN_MIRRORS" ]; then  
+    echo "Only $MIRROR_COUNT mirrors found..."
+    echo "Retrying with a longer timeout duration. This will be much slower. Please be patient..."
+    echo ""
+    rm -f "$TEMP_FILE"
+    rank_arch_mirrors "$TEMP_FILE" "$FALLBACK_PER_MIRROR_TIMEOUT"
+    ARCH_MIRROR_REFRESH_FAILED="$?"
+    MIRROR_COUNT=$(grep -oe '^Server\s*=\s*http' "$TEMP_FILE" | wc -l)
+    echo ""
+    echo ""
+fi
+
 ARCH_MIRROR_REFRESH_LOG_FILE="$TEMP_FILE"
-if [ "$ARCH_MIRROR_REFRESH_FAILED" -eq 0 ]; then    
+if [ "$ARCH_MIRROR_REFRESH_FAILED" -ne 0 ]; then
+    echo ""  
+    echo "ERROR: rate-mirrors exited with the error code $ARCH_MIRROR_REFRESH_FAILED..."
+    echo ""  
+elif [ "$MIRROR_COUNT" -lt "$MIN_MIRRORS" ]; then
+    ARCH_MIRROR_REFRESH_FAILED=-1
+    echo "ERROR: Only $MIRROR_COUNT mirrors found even with a longer timeout duration..."
+    echo "" 
+else    
     echo ""  
     set -o xtrace      
     pkexec cp -f "$TEMP_FILE" "$MIRRORLIST_FILE"
@@ -86,7 +150,7 @@ if [ "$REBORN_MIRROR_REFRESH_FAILED" -ne 0 ]; then
     echo ""    
     wait_and_exit 20 "$REBORN_MIRROR_REFRESH_FAILED"
 elif [ "$ARCH_MIRROR_REFRESH_FAILED" -ne 0 ]; then
-    echo "Refresh of Arch Linux mirrors failed with exit code: $REBORN_MIRROR_REFRESH_FAILED"
+    echo "Refresh of Arch Linux mirrors failed with exit code: $ARCH_MIRROR_REFRESH_FAILED"
     echo "Please check the log at: file://$ARCH_MIRROR_REFRESH_LOG_FILE"
     echo ""    
     wait_and_exit 20 "$ARCH_MIRROR_REFRESH_FAILED"
